6e4e8f2cd1367e8af84b8ed81a158a1d2912ad70
Sebastian Różyczka <rozyczka.sebastian@gmail.com>
2020-10-20 17:37:13 +0200

    commit: fix

--- src/PointCreator.java
+++ src/PointCreator.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class PointCreator {
-    Point create() {
+    public Point create() {
         Scanner scanner = new Scanner(System.in);
-        Point point = new Point();
 
         System.out.println("Podaj punkt X: ");
-        point.setX(scanner.nextInt());
+        int x = scanner.nextInt();
         scanner.nextLine();
         System.out.println("Podaj punkt Y: ");
-        point.setY(scanner.nextInt());
+        int y = scanner.nextInt();
 
+        Point point = new Point(x, y);
         return point;
     }
 }
